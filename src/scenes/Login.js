94316257ce6a7e8b5482e365a0bb5cf4c15a16d8
rchrdzeng <rchrdzeng@gmail.com>
2025-02-22 19:27:25 -0500

    changed_login_file

--- src/scenes/Login.js
+++ src/scenes/Login.js
@@ -18,7 +18,7 @@ export class Login extends Phaser.Scene
 
         const logo = this.add.image(640, 200, 'logo');
 
-        this.add.text(1920/2-96*2, 400-96, "Log in", { fontSize: "96px", fill: "#fff" });
+        let headerText = this.add.text(1920/2-96*2, 400-96, "Log in", { fontSize: "96px", fill: "#fff" });
 
         // Create username input
         let usernameInput = this.add.dom(1920/2-48, 600-96, 'input', {
@@ -36,6 +36,21 @@ export class Login extends Phaser.Scene
             fill: "#fff"
         });
 
+        let createButton = this.add.text(1920/2-48, 1100-96, "Create Account", {fontSize: "48px", fill: "#fff" })
+        .setInteractive()
+        .on('pointerdown', () => {
+            loginButton.value = "";
+            headerText.value = "Create Account";
+            let username = usernameInput.node.value;
+            let password = passwordInput.node.value;
+            if(username!="" && password!=""){
+                localStorage.setItem(username, password);
+            }
+            usernameInput.destroy();
+            passwordInput.destroy();
+            this.create();
+        });
+
         // Create login button
         let loginButton = this.add.text(1920/2-48, 1000-96, "Login", {fontSize: "48px", fill: "#fff" })
             .setInteractive()
@@ -43,7 +58,7 @@ export class Login extends Phaser.Scene
                 let username = usernameInput.node.value;
                 let password = passwordInput.node.value;
                 let loginText;
-                if (username === "admin" && password === "1234") {
+                if (localStorage.getItem(username) === password && username!= "" && password != "") {
                     usernameInput.destroy();
                     passwordInput.destroy();
                     loginButton.destroy();
@@ -52,6 +67,7 @@ export class Login extends Phaser.Scene
                     loginText = this.add.text(1920/2-96*2, 600, "Invalid login", { fontSize: "48px", fill: "#f00" });
                 }
             });
+
     }
 
     update()
