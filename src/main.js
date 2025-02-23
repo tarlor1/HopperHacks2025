dbe73c84f774450537032cfde95e62cb1cc13bdb
rchrdzeng <rchrdzeng@gmail.com>
2025-02-22 23:56:29 -0500

    a

--- src/main.js
+++ src/main.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-import { Start } from "./scenes/Start.js";
-import { Game } from "./scenes/Game.js"; // Import Game scene
-import { Login } from "./scenes/Login.js";
-import { UIScene } from "./scenes/UIScene.js";
-
-const config = {
-	type: Phaser.AUTO,
-	title: "Hopperhacks2",
-	parent: "game-container",
-	dom: {
-		createContainer: true,
-	},
-	width: 1920,
-	height: 1080,
-	pixelArt: true,
-	scene: [Start, Game, Login, UIScene], // Add all scenes here
-	scale: {
-		mode: Phaser.Scale.FIT,
-		autoCenter: Phaser.Scale.CENTER_BOTH,
-	},
-=======
 import { Start } from './scenes/Start.js';
 import { Game } from './scenes/Game.js';  // Import Game scene
 import { Login } from './scenes/Login.js';
@@ -40,7 +18,6 @@ const config = {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH
     },
->>>>>>> 5a2689e0c0eed22b469a630d3042fdabf47b1a99
 };
 
 new Phaser.Game(config);
