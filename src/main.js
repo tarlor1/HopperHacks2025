fa1922e60281808ff44be8d974a964a494614174
Vin1623 <vincenttan1623@gmail.com>
2025-02-22 19:47:22 -0500

    big channges

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 import { Start } from './scenes/Start.js';
 import { Game } from './scenes/Game.js';  // Import Game scene
+import { Login } from './scenes/Login.js';
 
 const config = {
     type: Phaser.AUTO,
     title: 'Hopperhacks2',
     parent: 'game-container',
+    dom: {
+        createContainer: true
+    },
     width: 1920,
     height: 1080,
     pixelArt: true,
-    scene: [ Start, Game ],  // Add all scenes here
+    scene: [ Start, Game, Login ],  // Add all scenes here
     scale: {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH
