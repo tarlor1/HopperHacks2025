99851cc3e7cf837a86baa24a1cd621423d063b87
tarlor1 <shaotarlor@gmail.com>
2025-02-22 23:55:25 -0500

    added habit tracker

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
 import { Start } from "./scenes/Start.js";
 import { Game } from "./scenes/Game.js"; // Import Game scene
 import { Login } from "./scenes/Login.js";
 import { UIScene } from "./scenes/UIScene.js";
+import { HabitTracking } from "./scenes/HabitTracking.js";
 
 const config = {
 	type: Phaser.AUTO,
@@ -14,33 +14,11 @@ const config = {
 	width: 1920,
 	height: 1080,
 	pixelArt: true,
-	scene: [Start, Game, Login, UIScene], // Add all scenes here
+	scene: [Start, Game, Login, UIScene, HabitTracking], // Add all scenes here
 	scale: {
 		mode: Phaser.Scale.FIT,
 		autoCenter: Phaser.Scale.CENTER_BOTH,
 	},
-=======
-import { Start } from './scenes/Start.js';
-import { Game } from './scenes/Game.js';  // Import Game scene
-import { Login } from './scenes/Login.js';
-import { HabitTracking } from './scenes/HabitTracking.js';
-
-const config = {
-    type: Phaser.AUTO,
-    title: 'Hopperhacks2',
-    parent: 'game-container',
-    dom: {
-        createContainer: true
-    },
-    width: 1920,
-    height: 1080,
-    pixelArt: true,
-    scene: [ Start, Login, Game, HabitTracking ],  // Add all scenes here
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-    },
->>>>>>> 5a2689e0c0eed22b469a630d3042fdabf47b1a99
 };
 
 new Phaser.Game(config);
